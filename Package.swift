490d35f4f940815b023bc6164329a19e5fcce79f
Yuvaraj M <yuvaraj.manickam@fiserv.com>
2025-05-09 01:08:50 +0530

    iOS CPSDK - v1.4.5
    We have updated the broken environment url

--- Package.swift
+++ Package.swift
@@ -14,17 +14,17 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PaymentSDK",
-            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.4.4/PaymentSDK_v1.4.4.xcframework.zip",
-            checksum: "44477f05b435dff3c183351c8761ced19db34159859fa196e36b35167fc40de3"
+            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.4.5/PaymentSDK_v1.4.5.xcframework.zip",
+            checksum: "384d9887d3c87c1790929070bcf8f7bd72c1f42ec62e16d819f2c6b8f2e9a9b1"
         ),
         .binaryTarget(
             name: "PayWithMyBank",
-            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.4.4/PayWithMyBank_v2.3.0.xcframework.zip",
+            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.4.5/PayWithMyBank_v2.3.0.xcframework.zip",
             checksum: "b467822c1976fd95a7e6139e4f3789e670d19a9b3f703b52ec4c6b9ea02db191"
         ),
         .binaryTarget(
             name: "RLTMXProfiling",
-            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.4.4/RLTMXProfiling_v6.3.70.xcframework.zip",
+            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.4.5/RLTMXProfiling_v6.3.70.xcframework.zip",
             checksum: "84a6e3395e0f981d14ad58e95459edbc24db555508d6b6255d0adc7dd30a38cf"
         ),
         .testTarget(
