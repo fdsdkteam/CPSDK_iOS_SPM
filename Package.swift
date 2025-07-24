fdbe397195450a1f205619c30802103919d9bbf2
Yuvaraj M <yuvaraj.manickam@fiserv.com>
2025-07-24 23:05:10 +0530

    v1.4.8 update
    SDK performance improvements

--- Package.swift
+++ Package.swift
@@ -14,17 +14,17 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PaymentSDK",
-            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.4.7/PaymentSDK_v1.4.7.xcframework.zip",
-            checksum: "ac7bacbfa2a8c7c4f914f0ab162ba19a14e83847e1344806eb4932164aa30629"
+            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.4.8/PaymentSDK_v1.4.8.xcframework.zip",
+            checksum: "df7eccb37cb9a0cd839f292e8c57220ed012522e7ad2da548a9c6e9ab8ca8a81"
         ),
         .binaryTarget(
             name: "PayWithMyBank",
-            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.4.7/PayWithMyBank_v2.3.0.xcframework.zip",
+            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.4.8/PayWithMyBank_v2.3.0.xcframework.zip",
             checksum: "b467822c1976fd95a7e6139e4f3789e670d19a9b3f703b52ec4c6b9ea02db191"
         ),
         .binaryTarget(
             name: "RLTMXProfiling",
-            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.4.7/RLTMXProfiling_v6.3.70.xcframework.zip",
+            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.4.8/RLTMXProfiling_v6.3.70.xcframework.zip",
             checksum: "84a6e3395e0f981d14ad58e95459edbc24db555508d6b6255d0adc7dd30a38cf"
         ),
         .testTarget(
