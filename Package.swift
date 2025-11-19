d22a922daad3207fc05b768ccf97f47983d63725
Yuvaraj M <yuvaraj.manickam@fiserv.com>
2025-11-20 00:04:39 +0530

    CPSDK v1.5.4 Release
    SDK performance improvements
    Default enrolment fi login page navigation bug fix

--- Package.swift
+++ Package.swift
@@ -21,17 +21,17 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PaymentSDK",
-            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.5.3/PaymentSDK_v1.5.3.xcframework.zip",
-            checksum: "fb735b3c387d6f5f3c666025bf95ad02bfd5549938230fa687f6c78029e19c1e"
+            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.5.4/PaymentSDK_v1.5.4.xcframework.zip",
+            checksum: "984083a1ea5e3078ec4a296ffeb56ca7e4c79068647410d32c57748c1407d8fd"
         ),
         .binaryTarget(
             name: "PayWithMyBank",
-            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.5.3/PayWithMyBank_v2.3.0.xcframework.zip",
+            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.5.4/PayWithMyBank_v2.3.0.xcframework.zip",
             checksum: "b467822c1976fd95a7e6139e4f3789e670d19a9b3f703b52ec4c6b9ea02db191"
         ),
         .binaryTarget(
             name: "RLTMXProfiling",
-            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.5.3/RLTMXProfiling_v6.3.70.xcframework.zip",
+            url: "https://github.com/fdsdkteam/CPSDK_iOS_SPM/releases/download/1.5.4/RLTMXProfiling_v6.3.70.xcframework.zip",
             checksum: "84a6e3395e0f981d14ad58e95459edbc24db555508d6b6255d0adc7dd30a38cf"
         ),
         .target(
